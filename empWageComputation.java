1d28807c3e6efd38802edce873d3ceeca37386f5
sujit gargate <sujitgargate5893@gmail.com>
2020-07-12 09:54:43 +0530

    Employee Present or Absent

--- empWageComputation.java
+++ empWageComputation.java
@@ -64,3 +64,22 @@ public class EmpPartFullTime{
 }
 
 //This is end of Part_Full time  //
+
+
+//This is end of FIle EmpPresentAbsent //
+
+public class EmpPresentAbsent{
+
+	public static void main(String args[]){
+	int IS_FULL_TIME=1;
+	double empCheck=Math.floor(Math.random()*10)%2;
+	if(empCheck==IS_FULL_TIME){
+		System.out.println("Employee is present ");
+		
+	}else{
+		System.out.println("Employee is absent ");
+	}
+	}
+}
+
+//This is end of FIle EmpPresentAbsent //
